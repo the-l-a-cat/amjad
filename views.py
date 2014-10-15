442af9646145d5dc8d6f83e181943948e68d642f
root <root@cat-free-09.14>
2014-10-15 15:51:55 -0400

    A basic server configured.

---
+++ views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404, render
+import os
+
+
+from xls import sheets, lift
+
+
+def index(request):
+    template = loader.get_template('/srv/django/amjad/amjad/front/index.html')
+    context = RequestContext (request)
+
+    # return HttpResponse("Hello, kitty.")
+    return HttpResponse(template.render(context))
+
+def api(request, *args):
+    return HttpResponse("meow")
+
+def js(request, js):
+    return HttpResponse (
+            loader.get_template (
+                os.path.join ( '/srv/django/amjad/amjad/front/js'
+                             , js)
+                                ) . render (RequestContext (request))
+                        )
+            
